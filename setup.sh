137466f302bc5aecc5424b8c9f2691aa2c3038ab
Harri Taylor <7202769+harritaylor@users.noreply.github.com>
2021-12-05 21:02:35 +0000

    added basic nvim config

--- setup.sh
+++ setup.sh
@@ -7,5 +7,9 @@ ln -sf "$PWD/inputrc" "$HOME/.inputrc"
 ln -sf "$PWD/profile" "$HOME/.profile"
 ln -sf "$PWD/profile" "$HOME/.bash_profile"
 
+
+# Programs that require a directory in the .config dir
+mkdir -p "$HOME/.config/"
+
 # Neovim
-ln -sf "$PWD/init.vim" "$HOME/.config/nvim"
+ln -sf "$PWD/nvim/" "$HOME/.config/"
--- teardown.sh
+++ teardown.sh
@@ -2,13 +2,10 @@
 set -e
 
 # Shell
-ln -sf "$PWD/.bashrc" "$HOME/.bashrc"
-ln -sf "$PWD/.inputrc" "$HOME/.inputrc"
-ln -sf "$PWD/.profile" "$HOME/.profile"
-ln -sf "$PWD/.profile" "$HOME/.bash_profile"
-
-# VSCode
-ln -sf "$PWD/settings.json" "$HOME/.config/Code/User"
+rm "$HOME/.bashrc"
+rm "$HOME/.inputrc"
+rm "$HOME/.profile"
+rm "$HOME/.bash_profile"
 
 # Neovim
-ln -sf "$PWD/init.vim" "$HOME/.config/nvim"
+rm -rf "$HOME/.config/nvim"
